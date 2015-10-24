3a48969af93286218481689bdac11c4634016dc9
desire2020 <desire2020@126.com>
2015-10-25 00:34:43 +0800

    Add & edit file(s).
    Our plan is uploaded.
    Database library edited.

--- database.hpp
+++ database.hpp
@@ -1,11 +1,15 @@
 #ifndef DEF_DATABASE
 #define DEF_DATABASE 
+#include <string>
 namespace NDatabase
 {
 	
 	class TLibrary
 	{
 		private:
+			TCharTree innerTree;
+	    public:
+			Tnode find(char *s);
 			
 	};
 }
