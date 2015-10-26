fa8adcdcf0fd52f062d989ae67959194f01513da
desire2020 <desire2020@126.com>
2015-10-26 11:58:52 +0800

    Edit file(s).
    To complete file "database.hpp".

--- database.hpp
+++ database.hpp
@@ -1,15 +1,24 @@
 #ifndef DEF_DATABASE
 #define DEF_DATABASE 
 #include <string>
+#include <map>
+#include <algorithm>
+
+
 namespace NDatabase
 {
-	
+	struct Tnode
+	{
+		string Title;
+		
+	}
+	typedef map<string,Tnode> TCharTree;
 	class TLibrary
 	{
 		private:
 			TCharTree innerTree;
 	    public:
-			Tnode find(char *s);
+			TCharTree find(char *s);
 			
 	};
 }
