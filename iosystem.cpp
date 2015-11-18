4601fa8a6c3da0d650153f287b1f95ca67e95ae7
Spy <simplespy@sina.cn>
2015-11-18 23:29:50 +0800

    Correct

--- iosystem.cpp
+++ iosystem.cpp
@@ -8,4 +8,28 @@ Headfile flag: DEF_IOSYSTEM;
 #ifndef DEF_IOSYSTEM
 #define DEF_IOSYSTEM
 
+namespace Nios{
+    
+    /*-------------------------------------*
+     * 函数 : SysDateStr()
+     * 功能 : 将当前系统时间转换为字符串
+     *-------------------------------------*/
+    
+    string SysDateStr()
+    {
+        char buf[128]= {0};
+        static string time_str;
+        time_t second;
+        tm* local; //本地时间
+        
+        second = time(NULL); //获取目前秒时间
+        local = localtime(&second); //转为本地时间
+        strftime(buf, 64, "%Y-%m-%d %H:%M:%S", local);
+        time_str = buf;
+        return time_str;
+    }
+
+    
+}
+
 #endif
--- log.cpp
+++ log.cpp
@@ -17,31 +17,10 @@
  11:改变用户状态。
  *********************************/
 #include "stdincs.h"
-#include <ctime>
-#include <stdlib.h>
-#include <cstdio>
-#include <string.h>
-#include <fstream>
+
 namespace Nlog
 {
     int Event;
-    char buf[128]= {0};
-    
-    /*-------------------------------------*
-     * 函数 : SysDateStr()
-     * 功能 : 将当前系统时间转换为字符串
-     *-------------------------------------*/
-    
-    string SysDateStr()
-    {
-        time_t second;
-        tm* local; //本地时间
-        
-        second = time(NULL); //获取目前秒时间
-        local = localtime(&second); //转为本地时间
-        strftime(buf, 64, "%Y-%m-%d %H:%M:%S", local);
-        return buf;
-    }
     
     /*-------------------------------------*
      * 函数 : RecordEvent(int Event, long long ID1<< long long ID2
@@ -50,55 +29,55 @@ namespace Nlog
     
     void RecordEvent(int Event, long long ID1, long long ID2)
     {
-        fout.open("syslog.log");
-        SysDateStr();
+        fileLogs.open("syslog.log");
+        fileLogs<<Nios::SysDateStr()<<"    ";
         switch (Event) {
             case 0:
-                fout<<"UserID : "<< ID1<<"    "<<"Registration Completed";
+                fileLogs<<"UserID : "<< ID1<<"    "<<"Registration Completed";
                 break;
             case 1:
-                fout<<"UserID : "<< ID1<< "    "<< "Login Completed";
+                fileLogs<<"UserID : "<< ID1<< "    "<< "Login Completed";
                 break;
             case 2:
-                fout<<"UserID : "<< ID1<< "    "<< "Logout Completed";
+                fileLogs<<"UserID : "<< ID1<< "    "<< "Logout Completed";
                 break;
             case 3:
-                fout<<"ISBN : "<< ID1<< "    is borrowed by   "<<"UserID : "<< ID2;
+                fileLogs<<"ISBN : "<< ID1<< "    is borrowed by   "<<"UserID : "<< ID2;
                 break;
             case 4:
-                fout<<"ISBN : "<< ID1<< "    is returned by   "<<"UserID : "<< ID2;
+                fileLogs<<"ISBN : "<< ID1<< "    is returned by   "<<"UserID : "<< ID2;
                 break;
             case 5:
                 if (ID1 != ID2)
-                    fout<<"UserID : "<< ID1<< "    Nickname is changed by   "<<"UserID : "<< ID2;
+                    fileLogs<<"UserID : "<< ID1<< "    Nickname is changed by   "<<"UserID : "<< ID2;
                 else
-                    fout<<"UserID : "<< ID1<< "    Nickname is changed by   "<<"AdminID : "<< ID2;
+                    fileLogs<<"UserID : "<< ID1<< "    Nickname is changed by   "<<"AdminID : "<< ID2;
                 break;
             case 6:
                 if (ID1 != ID2)
-                    fout<<"UserID : "<< ID1<< "    Passward is changed by   "<<"UserID : "<< ID2;
+                    fileLogs<<"UserID : "<< ID1<< "    Passward is changed by   "<<"UserID : "<< ID2;
                 else
-                    fout<<"UserID : "<< ID1<< "    Passward is changed by   "<<"AdminID : "<< ID2;
+                    fileLogs<<"UserID : "<< ID1<< "    Passward is changed by   "<<"AdminID : "<< ID2;
                 break;
             case 7:
-                fout<<"ISBN : "<< ID1<< "    is added by   "<<"AdminID : "<< ID2;
+                fileLogs<<"ISBN : "<< ID1<< "    is added by   "<<"AdminID : "<< ID2;
                 break;
             case 8:
-                fout<<"ISBN : "<< ID1<< "    is deleted by   "<<"AdminID : "<< ID2;
+                fileLogs<<"ISBN : "<< ID1<< "    is deleted by   "<<"AdminID : "<< ID2;
                 break;
             case 9:
-                fout<<"ISBN : "<< ID1<< "    is set by   "<<"AdminID : "<< ID2;
+                fileLogs<<"ISBN : "<< ID1<< "    is set by   "<<"AdminID : "<< ID2;
                 break;
             case 10:
-                fout<<"UserID : "<< ID1<< "    is deleted by   "<<"AdminID : "<< ID2;
+                fileLogs<<"UserID : "<< ID1<< "    is deleted by   "<<"AdminID : "<< ID2;
                 break;
             case 11:
-                fout<<"UserID : "<< ID1<< "    UserMode is changed by   "<<"AdminID : "<< ID2;
+                fileLogs<<"UserID : "<< ID1<< "    UserMode is changed by   "<<"AdminID : "<< ID2;
                 break;
                 
         }
-        fout<<"\n";
-        fout.close();
+        fileLogs<<"\n";
+        fileLogs.close();
     }
 }
 
