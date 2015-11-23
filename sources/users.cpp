2ba377c94bff4b963665d74f378f7eb0bed8d3f3
desire2020 <desire2020@126.com>
2015-11-23 22:40:17 +0800

    Debug
    Debug

--- sources/users.cpp
+++ sources/users.cpp
@@ -46,9 +46,9 @@ namespace Nusers
 		TInnerStruct :: iterator it;
 		it = UIDTree.find(tgUID);
 		UIDTree.erase(it);
-        remove(("/usersusersRB/" + Nios :: NumStr(tgUID) + ".log").c_str());
-        remove(("/usersusersInf/" + Nios :: NumStr(tgUID) + ".log").c_str());
-        remove(("/usersusersOccupiedBooks/" + Nios :: NumStr(tgUID) + ".log").c_str());
+        remove(("/users/usersRB/" + Nios :: NumStr(tgUID) + ".log").c_str());
+        remove(("/users/usersInf/" + Nios :: NumStr(tgUID) + ".log").c_str());
+        remove(("/users/usersOccupiedBooks/" + Nios :: NumStr(tgUID) + ".log").c_str());
 		return 0;
 	}
 	
